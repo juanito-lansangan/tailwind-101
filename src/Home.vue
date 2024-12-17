fa6964b849054f07458b186d41d54c7c17268317
Juan <juanito.lansangan@gmail.com>
2024-12-18 01:40:52 +0800

    update

--- src/Home.vue
+++ src/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1>Home</h1>
-  <ul>
-    <li>
-      <RouterLink class="p-2 text-blue-500 hover:underline" to="/simple-sidebar"
-        >Simple Sidebar</RouterLink
-      >
-    </li>
-  </ul>
+  <div class="p-4">
+    <h1 class="text-2xl">Tailwind lessons</h1>
+    <ul class="list-disc p-4">
+      <li>
+        <RouterLink
+          class="p-2 text-blue-500 hover:underline"
+          to="/simple-layout"
+          >Simple Layout</RouterLink
+        >
+      </li>
+    </ul>
+  </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
 import Home from "./Home.vue";
-import Index from "./lessons/simple-sidebar/Index.vue";
+import Index from "./lessons/simple-layout/Index.vue";
 import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
   { path: "/", component: Home },
-  { path: "/simple-sidebar", component: Index },
+  { path: "/simple-layout", component: Index },
 ];
 
 const router = createRouter({
